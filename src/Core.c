b4529b2ee5fbf884533a4a8d770fa8d769275361
Amine-jabote <114097461+Amine-jabote@users.noreply.github.com>
2022-12-08 16:03:25 +0100

    Update Core.c

--- src/Core.c
+++ src/Core.c
@@ -30,6 +30,7 @@ typedef struct Tasks {
     int duration;
     int earlier;
     int later;
+    int marge;
 } Tasks;
 /*
 * @brief Application de gantt_pert permet d'effectuer les calculs nécessaires pour gérer des diagrammes de Gantt et PERT
@@ -106,20 +107,91 @@ void calculate_earlier_later(Tasks *task, int *earlier, int *later) {
     *later = task->later;
 }
 
-void Identify_critical_tasks(Tasks* task, int* criticalTasks, int* criticalTaskCount) {
+bool Identify_critical_path(Tasks* task, int* criticalTasks, int* criticalTaskCount) {
     if (task->earlier == task->later) {
         criticalTasks[(*criticalTaskCount)++] = task->id;
-    for (int i = 0; i < task->successorCount; i++) {
-        Identify_critical_tasks(task->successors[i], criticalTasks, criticalTaskCount);
+        found_critical = false;
+        for (int i = 0; i < task->successorCount && !found_critical; i++ ) {
+            found_critical = Identify_critical_tasks(task->successors[i], criticalTasks, criticalTaskCount);
+        }
+        return true;
     }
-    }
+    return false;
 
 }
 
 
+void calculate_marge(Tasks* task, int* marge) {
+    if (task->earlier != task->later) {
+        task->marge = task->later - task->earlier;
+    }
+    elif (task->earlier == task->later) {
+         task->marge= 0;
+    }
+    for (int i = 0; i < task->successorCount; i++) {
+        calculate_marge(task->successors[i], marge);
+    }
+}
+
+void Gantt_chart(Tasks* task, int* Gantt, int* ordreCount) {
+    #vérifier si task fait partie de la liste des tâches critiques
+    if (task->earlier == task->later) {
+        Gantt[task->index] = task->id;
+    }
+    for (int i = 0; i < task->successorCount; i++) {
+        Gantt_chart(task->successors[i], Gantt, ordreCount);
+    }
+}
 
 
 int main()
-{   
- return 0;
+{
+    Tasks* taskSucc1[1];
+    Tasks* taskSucc2[2];
+    Tasks* taskSucc3[2];
+    Tasks* taskSucc4[1];
+    Tasks* taskSucc5[1];
+    Tasks* taskSucc6[1];
+    Tasks* taskSucc7[1];
+    Tasks* taskSucc8[0];
+    Tasks* taskSucc9[1];
+    Tasks* taskSucc10[1];
+    Tasks* taskAnc1[0];
+    Tasks* taskAnc2[1];
+    Tasks* taskAnc3[1];
+    Tasks* taskAnc4[1];
+    Tasks* taskAnc5[1];
+    Tasks* taskAnc6[2];
+    Tasks* taskAnc7[2];
+    Tasks* taskAnc8[1];
+    Tasks* taskAnc9[1];
+    Tasks* taskAnc10[1];
+    Tasks task1 = {.id=1, .successors=taskSucc1, .ancestors=taskAnc1};
+    Tasks task2 = {.id=2, .successors=taskSucc2, .ancestors=taskAnc2};
+    Tasks task3 = {.id=3, .successors=taskSucc3, .ancestors=taskAnc3};
+    Tasks task4 = {.id=4, .successors=taskSucc4, .ancestors=taskAnc4};
+    Tasks task5 = {.id=5, .successors=taskSucc5, .ancestors=taskAnc5};
+    Tasks task6 = {.id=6, .successors=taskSucc6, .ancestors=taskAnc6};
+    Tasks task7 = {.id=7, .successors=taskSucc7, .ancestors=taskAnc7};
+    Tasks task8 = {.id=8, .successors=taskSucc8, .ancestors=taskAnc8};
+    Tasks task9 = {.id=9, .successors=taskSucc9, .ancestors=taskAnc9};
+    Tasks task10 = {.id=10, .successors=taskSucc10, .ancestors=taskAnc10};
+    
+    add_successor(&task1, &task2);
+    add_successor(&task2, &task3);
+    add_successor(&task3, &task4);
+    add_successor(&task4, &task6);
+    add_successor(&task3, &task5);
+    add_successor(&task5, &task6);
+    add_successor(&task6, &task7);
+    add_successor(&task7, &task8);
+    add_successor(&task2, &task10);
+    add_successor(&task10, &task9);
+    add_successor(&task9, &task7);
+    
+    int firstTaskIndex = 0, lastTaskIndex = 9;
+    fill_indice(&task1, &task8, &firstTaskIndex, &lastTaskIndex);
+    printf("%d %d %d %d %d %d %d %d %d %d", task1.index, task2.index, task3.index, task4.index, task5.index, task6.index, task7.index, task8.index, task9.index, task10.index);
+    
+    return 0;
 }
