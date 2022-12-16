97f12d24aabd14d7be3865cbbcbb942890338705
Teddy Roncin <teddy.roncin35@gmail.com>
2022-12-16 17:24:08 +0100

    changed style of gantt widget

--- src/render/widget/GanttTaskWidget.py
+++ src/render/widget/GanttTaskWidget.py
@@ -20,35 +20,32 @@ class GanttTaskWidget(Widget):
         self.parent_bb = parent_bb
         self.get_y_offset = get_y_offset
         # The raw position of the widget before the scrolling is applied
-        self.pos = self.parent_bb.x, self.parent_bb.y + (self.task.index-1) * 50
+        self.pos = self.parent_bb.x, self.parent_bb.y + 20 + (self.task.index-1) * 50
         # The raw size of the widget before the cropping (due to scrolling) is applied
         self.size = self.parent_bb.width, 50
+        # The size of the timeline part of the widget.
+        # We leave 100 pixels to display the name, and 3 pixels up and down for a small margin between each task
+        self.timeline_size = self.size[0] - 100, self.size[1] - 6
         # The height of the widget that is cropped because of scrolling (only on top)
         self.amount_cropped = 0
         self.colors = self.COLORS[(self.task.index - 1) % len(self.COLORS)]
-        self.font = pygame.font.SysFont("Arial", 8)
+        font = pygame.font.SysFont("Arial", 12)
+        self.task_name_render = font.render(self.task.name, True, 0x000000)
 
     def draw(self, surface):
+        surface.blit(self.task_name_render, (0, (50 - self.task_name_render.get_height()) / 2 - self.amount_cropped))
         if self.total_time == 0:
             pygame.draw.rect(surface,
                              self.colors[0],
-                             pygame.Rect(0, 3 - self.amount_cropped, self.size[0], self.size[1] - 6))
-            surface.blit(self.font.render(self.task.name, True, (0, 0, 0)), (0, 3 - self.amount_cropped))
+                             pygame.Rect((100, 3 - self.amount_cropped), self.timeline_size))
         else:
             pygame.draw.rect(surface,
                              self.colors[0],
-                             pygame.Rect(self.task.earliest_start / self.total_time * self.size[0],
+                             pygame.Rect(100 + self.task.earliest_start / self.total_time * self.timeline_size[0],
                                          3 - self.amount_cropped,
-                                         (self.task.latest_start - self.task.earliest_start + self.task.estimated_time) / self.total_time * self.size[0],
-                                         self.size[1] - 6))
-            surface.blit(self.font.render(self.task.name, True, (0, 0, 0)),
-                         (self.task.earliest_start / self.total_time * self.size[0], 3-self.amount_cropped))
-        #pygame.draw.rect(surface,
-        #                 self.colors[1],
-        #                 pygame.Rect(self.task.earliest_start / self.total_time * self.size[0], 3-self.amount_cropped, (self.task.latest_start-self.task.earliest_start) / self.total_time * self.size[0], self.size[1] - 6))
-        #pygame.draw.rect(surface,
-        #                 self.colors[1],
-        #                 pygame.Rect((self.task.earliest_start + self.task.estimated_time) / self.total_time * self.size[0], 3-self.amount_cropped, (self.task.latest_start - self.task.earliest_start) / self.total_time * self.size[0], self.size[1] - 6))
+                                         (self.task.latest_start - self.task.earliest_start
+                                          + self.task.estimated_time) / self.total_time * self.timeline_size[0],
+                                         self.timeline_size[1]))
 
     def get_bb(self):
         y_offset = self.get_y_offset()
--- src/render/widget/GanttWidget.py
+++ src/render/widget/GanttWidget.py
@@ -11,11 +11,13 @@ class GanttWidget(Widget):
         super().__init__()
         self.project = project
         self.bb = pygame.Rect(600, 100, 500, 300)
-        self.scroll_bar = ScrollBarWidget(self.get_bb, lambda: project.tasks_count*50)
+        self.scroll_bar = ScrollBarWidget(self.get_bb, lambda: project.tasks_count*50 + 50)
         self.y_offset = 0
         self.task_widgets = []
         self.total_time = self.project.project_task.earliest_start + self.project.project_task.estimated_time
         self.generate_widgets(self.project.beginning_task)
+        self.number_of_time_marks = (project.tasks_count // 5) % 5 + 5
+        self.font = pygame.font.SysFont("Arial", 12)
 
     def get_children(self):
         yield from self.task_widgets
@@ -23,6 +25,14 @@ class GanttWidget(Widget):
 
     def draw(self, surface):
         surface.fill(0xffffff)
+        for i in range(self.number_of_time_marks):
+            time_mark = self.font.render(str((self.project.project_task.latest_start
+                                         + self.project.project_task.estimated_time) * i / self.number_of_time_marks),
+                                         False,
+                                         0x000000)
+            pos = 100 + (self.bb.width - 100) * i / self.number_of_time_marks
+            surface.blit(time_mark, (pos - time_mark.get_width() / 2, 3))
+            pygame.draw.line(surface, 0x000000, (pos, 20), (pos, self.bb.height))
 
     def reload(self):
         self.generate_widgets(self.project.beginning_task)
